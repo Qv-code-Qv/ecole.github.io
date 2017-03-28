01a11b32aa9cd2d440b1c81b524727269045f2b5
Quentin Vilcoque <q.vilcoque@gmail.com>
2017-03-28 13:05:16 +0200

    connection
    - connectionBase : requete pour se connecté à la bdd
    -ConnectionEleveProf : jointure pour que l'eleve et le prof puisse se
    connecter

---
+++ connectionEleveProf.php
@@ -0,0 +1,37 @@
+<?php
+
+$titre="Connection";
+include("connectionBase.php");
+
+// Hachage du mot de passe
+$password = sha1($_POST['password']);
+$login=$_POST['login'];
+
+// Vérification des identifiants
+$req = $bdd->prepare('SELECT * FROM Eleve INNER JOIN prof
+on eleve.idprof = prof.idprof WHERE Login  = :login AND  Mot_De_Passe = :password');
+$req->execute(array(
+'login' => $login,
+'password' => $password));
+
+$resultat = $req->fetch();
+
+if(!$resultat)
+    {
+        echo 'Veuillez reessayer';
+        header('refresh:3;../index.html');
+        exit();
+    }
+else
+    {
+
+        session_start();
+
+
+        echo 'Vous etes connecte !';
+
+        header('refresh:3;');
+        exit();
+    }
+
+?>
